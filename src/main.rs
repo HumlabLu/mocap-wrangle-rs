66fd4aea9eada7f232c22803735ebd4bc8d70d11
Peter Berck <peter.berck@humlab.lu.se>
2023-05-05 12:56:17 +0200

    Wrapped test cases in own space.

--- src/main.rs
+++ src/main.rs
@@ -358,91 +358,95 @@ fn create_outputfilename(filename: &str) -> String {
 // Tests.
 // =====================================================================
 
-#[test]
-fn test_zero_dist() {
-    let dist = dist_3d(&[0.0,0.0,0.0], &[0.0,0.0,0.0]);
-    assert!(dist==0.0);
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_zero_dist() {
+	let dist = dist_3d(&[0.0,0.0,0.0], &[0.0,0.0,0.0]);
+	assert!(dist==0.0);
+    }
 
-#[test]
-fn test_normal_dist() {
-    let dist = dist_3d(&[1.0,0.0,0.0], &[0.0,0.0,0.0]);
-    assert!(dist==1.0);
-}
+    #[test]
+    fn test_normal_dist() {
+	let dist = dist_3d(&[1.0,0.0,0.0], &[0.0,0.0,0.0]);
+	assert!(dist==1.0);
+    }
 
-#[test]
-#[should_panic]
-fn test_wrong_params_lhs() {
-    let dist = dist_3d(&[1.0,0.0,0.0,4.0], &[0.0,0.0,0.0]);
-}
+    #[test]
+    #[should_panic]
+    fn test_wrong_params_lhs() {
+	let dist = dist_3d(&[1.0,0.0,0.0,4.0], &[0.0,0.0,0.0]);
+    }
 
-#[test]
-fn test_dist_wrong_params_rhs() {
-    let result = std::panic::catch_unwind(|| dist_3d(&[1.0,0.0,0.0], &[0.0,0.0,0.0,4.0]));
-    assert!(result.is_err()); 
-}
+    #[test]
+    fn test_dist_wrong_params_rhs() {
+	let result = std::panic::catch_unwind(|| dist_3d(&[1.0,0.0,0.0], &[0.0,0.0,0.0,4.0]));
+	assert!(result.is_err()); 
+    }
 
-#[test]
-fn filename_normal() {
-    let result = create_outputfilename("filename.tsv");
-    assert!(result=="filename_d3D.tsv");
-}
+    #[test]
+    fn filename_normal() {
+	let result = create_outputfilename("filename.tsv");
+	assert!(result=="filename_d3D.tsv");
+    }
 
-#[test]
-fn filename_short() {
-    let result = create_outputfilename("");
-    assert!(result=="output_d3D.tsv");
-}
+    #[test]
+    fn filename_short() {
+	let result = create_outputfilename("");
+	assert!(result=="output_d3D.tsv");
+    }
 
-#[test]
-fn filename_four_chars() {
-    let result = create_outputfilename("abcd");
-    assert!(result=="output_d3D.tsv");
-}
+    #[test]
+    fn filename_four_chars() {
+	let result = create_outputfilename("abcd");
+	assert!(result=="output_d3D.tsv");
+    }
 
-#[test]
-fn filename_five_chars() {
-    let result = create_outputfilename("a.tsv");
-    assert!(result=="a_d3D.tsv");
-}
+    #[test]
+    fn filename_five_chars() {
+	let result = create_outputfilename("a.tsv");
+	assert!(result=="a_d3D.tsv");
+    }
 
-#[test]
-fn filename_no_tsv() {
-    let result = create_outputfilename("abcde");
-    assert!(result=="abcde_d3D.tsv");
-}
+    #[test]
+    fn filename_no_tsv() {
+	let result = create_outputfilename("abcde");
+	assert!(result=="abcde_d3D.tsv");
+    }
 
-#[test]
-fn struct_is_valid() {
-    let mut myfile = MoCapFile {
-	name: String::new(),
-	no_of_frames: 0,
-	no_of_cameras: 0,
-	no_of_markers: 0,
-	frequency: 200,
-	no_of_analog: 0,
-	description: String::new(),
-	time_stamp: String::new(),
-	data_included: String::new(),
-	marker_names: vec!["X".to_string()],
-    };
-    assert!(myfile.is_valid()==true);
-}
+    #[test]
+    fn struct_is_valid() {
+	let mut myfile = MoCapFile {
+	    name: String::new(),
+	    no_of_frames: 0,
+	    no_of_cameras: 0,
+	    no_of_markers: 0,
+	    frequency: 200,
+	    no_of_analog: 0,
+	    description: String::new(),
+	    time_stamp: String::new(),
+	    data_included: String::new(),
+	    marker_names: vec!["X".to_string()],
+	};
+	assert!(myfile.is_valid()==true);
+    }
 
-#[test]
-fn struct_is_invalid() {
-    let mut myfile = MoCapFile {
-	name: String::new(),
-	no_of_frames: 0,
-	no_of_cameras: 0,
-	no_of_markers: 0,
-	frequency: 0,
-	no_of_analog: 0,
-	description: String::new(),
-	time_stamp: String::new(),
-	data_included: String::new(),
-	marker_names: vec![],
-    };
-    assert!(myfile.is_valid()==false);
+    #[test]
+    fn struct_is_invalid() {
+	let mut myfile = MoCapFile {
+	    name: String::new(),
+	    no_of_frames: 0,
+	    no_of_cameras: 0,
+	    no_of_markers: 0,
+	    frequency: 0,
+	    no_of_analog: 0,
+	    description: String::new(),
+	    time_stamp: String::new(),
+	    data_included: String::new(),
+	    marker_names: vec![],
+	};
+	assert!(myfile.is_valid()==false);
+    }
 }
-
