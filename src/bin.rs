340a4af79120c0362fa76bef3802f0a19b0685a8
Peter Berck <peter.berck@humlab.lu.se>
2023-05-05 08:59:11 +0200

    Renamed main.rs to bin.rs, added lib.rs.

--- src/bin.rs
+++ src/bin.rs
@@ -10,6 +10,9 @@ use clap::Parser;
 use std::path::Path;
 use std::time::{Duration, Instant};
 
+use mocap::dist_3d;
+use mocap::SensorFloat;
+
 #[macro_use] extern crate log;
 extern crate simplelog;
 use simplelog::*;
@@ -47,11 +50,6 @@ struct Args {
 // =====================================================================
 // Main.
 // =====================================================================
-
-/// The float type for the coordinates/velocities, etc.
-/// Note that there are different sizes of integers as well.
-/// # (Maybe use usize for all the integers?)
-type SensorFloat = f32;
 
 /// Reads the MoCap file with sensor data.
 ///
@@ -336,32 +334,11 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-/// Calculate the distance in 3D.
-///
-/// # Example
-///
-/// ```rust
-/// let dist = dist_3d(&[1.0, 0.0, 0.0], &[0.0, 0.0, 0.0]);
-/// ```
-fn dist_3d(coords0: &[SensorFloat], coords1: &[SensorFloat]) -> SensorFloat {
-    assert!(coords0.len() == 3);
-    assert!(coords1.len() == 3);
-    /*
-    let x_diff = coords0[0] - coords1[0];
-    let y_diff = coords0[1] - coords1[1];
-    let z_diff = coords0[2] - coords1[2];
-    let distance_squared = x_diff * x_diff + y_diff * y_diff + z_diff * z_diff;
-    distance_squared.sqrt()
-    */
-    let squared_sum = coords0.iter()
-        .zip(coords1.iter())
-        .map(|(&a, &b)| (a - b) * (a - b))
-        .fold(0.0, |acc, x| acc + x);
-    squared_sum.sqrt()
-}
-
 /// Create a new output filename, tries to append "_d3D" to
 /// the filename.
+///
+/// Short input filenames will return `output_d3D.tsv`.
+///
 fn create_outputfilename(filename: &str) -> String {
     let len = filename.len();
     if len > 4 { 
---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+/// The float type for the coordinates/velocities, etc.
+/// Note that there are different sizes of integers as well.
+/// # (Maybe use usize for all the integers?)
+pub type SensorFloat = f32;
+
+/// Calculate the distance in 3D.
+///
+/// # Example
+///
+/// ```rust
+/// let dist = dist_3d(&[1.0, 0.0, 0.0], &[0.0, 0.0, 0.0]);
+/// ```
+pub fn dist_3d(coords0: &[SensorFloat], coords1: &[SensorFloat]) -> SensorFloat {
+    assert!(coords0.len() == 3);
+    assert!(coords1.len() == 3);
+    /*
+    let x_diff = coords0[0] - coords1[0];
+    let y_diff = coords0[1] - coords1[1];
+    let z_diff = coords0[2] - coords1[2];
+    let distance_squared = x_diff * x_diff + y_diff * y_diff + z_diff * z_diff;
+    distance_squared.sqrt()
+    */
+    let squared_sum = coords0.iter()
+        .zip(coords1.iter())
+        .map(|(&a, &b)| (a - b) * (a - b))
+        .fold(0.0, |acc, x| acc + x);
+    squared_sum.sqrt()
+}
