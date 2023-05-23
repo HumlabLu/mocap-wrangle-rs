96b66b2301f028d5e79aa30eae963a79727520ed
Peter Berck <peter.berck@humlab.lu.se>
2023-05-23 15:30:50 +0200

    Moving main code to new function. WIP.

--- src/main.rs
+++ src/main.rs
@@ -267,6 +267,163 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn read_and_write(args: Args) {
+
+    let filename = args.file;
+    let file_size = std::fs::metadata(&filename).unwrap().len();
+    if file_size < 28 { // Arbitrary size... but to prevent creation of 0-byte files.
+	error!("Error: Inputfile is too small!");
+	std::process::exit(2);
+    }
+    let file = File::open(filename.clone()).expect("could not open file");
+    let fileiter = std::io::BufReader::new(file).lines();
+
+    let outfilename = if args.fileout.is_some() {
+	args.fileout.unwrap() // unwrap() to get the value, which we know exists.
+    } else {
+	create_outputfilename(&filename)
+    };
+    if !args.force && Path::new(&outfilename).exists() == true {
+	error!("Error: {} exists! Use --force to overwrite.", outfilename);
+	std::process::exit(1);
+	// Or use Ulid to generate a filename? (https://github.com/huxi/rusty_ulid)
+    }
+    let mut file_out = File::create(&outfilename).unwrap();
+    let mut buffer_out = BufWriter::new(file_out);
+    
+    info!("Reading file {}", filename);
+    info!("Writing file {}", outfilename);
+
+    // Metadata structure.
+    let mut myfile = MoCapFile { name: filename, ..Default::default() };
+    
+    let mut line_no: usize = 0; // Counts line in the file.
+    let mut frames_no: usize = 0; // Counts the lines with sensor data.
+    let mut prev_bits: Option<Vec<SensorFloat>> = None; // Previous line used in calculations.
+    let mut prev_slice: &[SensorFloat] = &[0.0, 0.0, 0.0]; // Previous X, Y and Z coordinates.
+    let mut wrote_header = !args.header; // If we specify --header, wrote_header becomes false.
+    let mut output_bits = Vec::<SensorFloat>::new(); // Sensor values as SensorFloat.
+    
+    let time_start = Instant::now();
+    
+    for line in fileiter {
+        if let Ok(l) = line {
+            //println!("{}", l);
+	    if l.len() < 1 {
+		continue;
+	    }
+	    let ch = &l.chars().take(1).last().unwrap(); // Surely, this could be simplified?!
+	    if ch.is_ascii_uppercase() { // Assume we are still parsing the header.
+		if args.verbose {
+		    info!("{}", l);
+		}
+		// Some matching for testing.
+		if let Some(x) = mocap::extract_no_of_frames(&l) {
+		    myfile.no_of_frames = x;
+		}
+		if let Some(x) = mocap::extract_no_of_cameras(&l) {
+		    myfile.no_of_cameras = x;
+		}
+		if let Some(x) = mocap::extract_no_of_markers(&l) {
+		    myfile.no_of_markers = x;
+		}		
+		if let Some(x) = mocap::extract_frequency(&l) {
+		    myfile.frequency = x;
+		}
+		if let Some(x) = mocap::extract_marker_names(&l) {
+		    myfile.marker_names = x;
+		}		
+		if let Some(x) = mocap::extract_time_stamp(&l) {
+		    myfile.time_stamp = x;
+		}		
+		if let Some(x) = mocap::extract_description(&l) {
+		    myfile.description = x;
+		}		
+	    } 
+	    else { // Assume we are in the data part.
+		if !myfile.is_valid() {
+		    error!("The file does not seem to contain a header!");
+		    break; // This creates a zero byte file.
+		}
+		// If we requested a header, print it first (at this point we have not
+		// written to the output file yet.
+		//
+		if !wrote_header {
+		    let mut output_strs = Vec::new();
+		    for marker_name in &myfile.marker_names {
+			for marker_type in vec!["_X", "_Y", "_X", "_d3D"] {
+			    //print!("{}{}", marker_name, marker_type );
+			    output_strs.push(format!("{}{}", marker_name, marker_type));
+			}
+		    }
+		    for (i, value) in output_strs.iter().enumerate() {
+			if i > 0 {
+			    buffer_out.write(b"\t").unwrap(); // If not at start, write a tab.
+			}
+			buffer_out.write_fmt(format_args!("{}", value)).unwrap();
+		    }
+		    buffer_out.write(b"\n").unwrap();
+		    wrote_header = true;
+		}
+		    
+		//let bits: Vec<&str> = l.split("\t").collect();
+		let mut bits = l.split("\t").
+		    filter_map(
+			|s| s.parse::<SensorFloat>().ok() // We assume all SensorFloat values for now.
+		    ).collect::<Vec<_>>();
+		if args.skip > 0 {
+		    //let u: Vec<_> = bits.drain(0..args.skip).collect(); // Keep them, output them?
+		    bits.drain(0..args.skip);
+		}
+		let num_bits = bits.len(); // Should be 3 * marker_names.len()
+		let expected_num_bits = (myfile.no_of_markers * 3) as usize;
+		if num_bits > expected_num_bits {
+		    // Two extra fields could mean a frame number and frame time!
+		    let num_extra = num_bits - expected_num_bits;
+		    info!("Got {} extra fields in line {}, skipping (or use -s{})!",
+			  num_extra, line_no, num_extra);
+		} else if num_bits < expected_num_bits {
+		    info!("Got {} ({}) missing fields in line {}, skip!",
+			  expected_num_bits - num_bits, expected_num_bits, line_no);
+		} else {
+		    //let mut output_bits = Vec::new(); // Collect and save values at the end.
+		    for triplet in (0..num_bits).step_by(3) { // Process per triple.
+			let slice = &bits[triplet..triplet+3];
+			if prev_bits.is_some() { // Do we have a saved "previous line/triplet"?
+			    let x = prev_bits.clone().unwrap();
+			    prev_slice = &x[triplet..triplet+3];
+			    let dist = dist_3d(slice, prev_slice);
+			    //println!("{} {:?} {:?} {}", frames_no, slice, prev_slice, dist);
+			    //write!(file_out, "{}\t{}\t{}\t{}", slice[0], slice[1], slice[2], dist);
+			    output_bits.extend_from_slice(&slice);
+			    output_bits.push(dist);
+			} else {
+			    // No previous bits, the dist is 0 (our starting value).
+			    prev_slice = &slice;
+			    let dist = 0.0;
+			    //println!("{} {:?} {:?} {}", frames_no, slice, prev_slice, dist);
+			    //write!(file_out, "{}\t{}\t{}\t{}", slice[0], slice[1], slice[2], dist);
+			    output_bits.extend_from_slice(&slice);
+			    output_bits.push(dist);
+			}
+		    }
+		    prev_bits = Some(bits);
+		    frames_no += 1;
+		    for (i, value) in output_bits.iter().enumerate() {
+			if i > 0 {
+			    buffer_out.write(b"\t").unwrap();
+			}
+			buffer_out.write_fmt(format_args!("{:.3}", value)).unwrap(); // Note the ".3"!
+		    }
+		    buffer_out.write(b"\n").unwrap();
+		    output_bits.clear();
+		}
+	    } // If sensor data.
+	    line_no += 1;
+        }
+    }
+}
+
 /// Create a new output filename, tries to append "_d3D" to
 /// the filename.
 ///
